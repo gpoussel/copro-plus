2fc91b12cbabd2552fb55ab731ebb7029dc8b1d1
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-05 15:04:31 +0100

    Update template and C++ standard in VS Code

--- templates/problem.cpp
+++ templates/problem.cpp
@@ -1,9 +1,21 @@
 #include <algorithm>
+#include <array>
+#include <cassert>
 #include <cmath>
 #include <cstdio>
+#include <iomanip>
 #include <iostream>
+#include <numbers>
+#include <set>
+#include <string>
 #include <vector>
 using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+#define equals(a, b) (fabs(a - b) < EPSILON)
+#define almost_equals(a, b) (fabs(a - b) < 1e-4)
 
 // HEADER
 
