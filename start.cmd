5b690ae94b6c07142bd8b734b67e3b1c0e716caa
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-05 12:56:01 +0100

    Update Windows Terminal layout

--- start.cmd
+++ start.cmd
@@ -1,7 +1,7 @@
 @ECHO off
 SETLOCAL
 
-call wt --size 160,50 --title "Watcher" -d "./" %~dp0\watch.cmd ; split-pane --title "Problem Solving" --tabColor #0078D4 -V -d %~dp0 ; new-tab -d %~dp0 --title "Miscellaneous" --tabColor #404040 ; focus-tab --target 0
+call wt --size 200,50 --title "Watcher" -d "./" %~dp0\watch.cmd ; split-pane --size .7 --title "Problem Solving" --tabColor #0078D4 -V -d %~dp0 ; new-tab -d %~dp0 --title "Miscellaneous" --tabColor #404040 ; focus-tab --target 0
 
 ENDLOCAL
 
