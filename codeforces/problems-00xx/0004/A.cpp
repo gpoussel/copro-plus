72b8db998f070e90a5676fa836fb38b846f53026
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-05 17:26:07 +0100

    Solved Codeforces 4A

---
+++ codeforces/problems-00xx/0004/A.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+#define equals(a, b) (fabs(a - b) < EPSILON)
+#define almost_equals(a, b) (fabs(a - b) < 1e-4)
+
+// Codeforces - Codeforces Beta Round 4 (Div. 2 Only)
+// A. Watermelon
+
+int main()
+{
+    int w;
+    cin >> w;
+    cout << ((w > 2 && w % 2 == 0) ? "YES" : "NO") << nl;
+    return 0;
+}
