356bf196460841aa75618cadc3a5a21ea8692f5b
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-06 20:24:14 +0100

    Remove useless escape sequences

--- commit.cmd
+++ commit.cmd
@@ -1,9 +1,6 @@
 @ECHO off
 SETLOCAL
 
-FOR /F %%a in ('echo prompt $E ^| cmd') DO SET "ESC=%%a"
-
-
 where /q python
 IF ERRORLEVEL 1 (
     echo %ESC%[91mThis executable needs python.exe in the PATH%ESC%[0m
