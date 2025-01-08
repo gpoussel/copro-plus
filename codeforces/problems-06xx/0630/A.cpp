3832e05d729b225db955dd964bb4a1a6e61b1e05
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-08 23:21:25 +0100

    Solved Codeforces 0630A

---
+++ codeforces/problems-06xx/0630/A.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Experimental Educational Round: VolBIT Formulas Blitz
+// A. Again Twenty Five!
+
+int main()
+{
+    cout << 25 << nl;
+    return 0;
+}
