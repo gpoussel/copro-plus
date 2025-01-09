2953f24b75956104e00504f94529e4664b4c676c
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-09 23:25:31 +0100

    Solved Codeforces 1367A

---
+++ codeforces/problems-13xx/1367/A.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 650 (Div. 3)
+// A. Short Substrings
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        string str;
+        cin >> str;
+
+        for (int i = 0; i < (int)str.size(); i += 2)
+        {
+            cout << str[i];
+        }
+        cout << str.back();
+        cout << nl;
+    }
+    return 0;
+}
