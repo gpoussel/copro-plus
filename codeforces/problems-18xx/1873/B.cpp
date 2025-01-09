44f3ee7f92e616812feaa9b762b2afff6bd8d4b1
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-09 23:25:34 +0100

    Solved Codeforces 1873B

---
+++ codeforces/problems-18xx/1873/B.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 898 (Div. 4)
+// B. Good Kid
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        vector<int> arr(length);
+        for (int j = 0; j < length; j++)
+        {
+            cin >> arr[j];
+        }
+        sort(arr.begin(), arr.end());
+        arr[0]++;
+        int result = accumulate(arr.begin(), arr.end(), 1, multiplies<int>());
+        cout << result << nl;
+    }
+    return 0;
+}
