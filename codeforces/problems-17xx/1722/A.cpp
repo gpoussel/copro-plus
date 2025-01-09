354a64770710e0bfea113c27d0f8c0e49caeef40
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-09 23:25:33 +0100

    Solved Codeforces 1722A

---
+++ codeforces/problems-17xx/1722/A.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 817 (Div. 4)
+// A. Spell Check
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        sort(str.begin(), str.end());
+        cout << (str == "Timru" ? "YES" : "NO") << nl;
+    }
+    return 0;
+}
