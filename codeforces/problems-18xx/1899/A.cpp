d6171ee607002729c34885645f100b225cbcc770
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-10 22:54:28 +0100

    Solved Codeforces 1899A

---
+++ codeforces/problems-18xx/1899/A.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 909 (Div. 3)
+// A. Game with Integers
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        if (n % 3 != 0)
+        {
+            cout << "First" << nl;
+        }
+        else
+        {
+            cout << "Second" << nl;
+        }
+    }
+    return 0;
+}
