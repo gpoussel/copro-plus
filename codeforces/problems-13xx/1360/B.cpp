1c99f486a51a1f6968d775331812c28685575e6e
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-10 22:54:27 +0100

    Solved Codeforces 1360B

---
+++ codeforces/problems-13xx/1360/B.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 644 (Div. 3)
+// B. Honest Coach
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        vector<int> arr(length);
+        for (int j = 0; j < length; j++)
+        {
+            cin >> arr[j];
+        }
+        sort(arr.begin(), arr.end());
+        int min_diff = INT_MAX;
+        for (int j = 1; j < length; j++)
+        {
+            min_diff = min(min_diff, arr[j] - arr[j - 1]);
+        }
+        cout << min_diff << nl;
+    }
+    return 0;
+}
