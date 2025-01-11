f70e6b3c8dbb647934748c9c02034f355ea530f2
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-11 11:06:33 +0100

    Solved Codeforces 1996A

---
+++ codeforces/problems-19xx/1996/A.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 962 (Div. 3)
+// A. Legs
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        cout << n / 4 + (n % 4) / 2 << nl;
+    }
+    return 0;
+}
