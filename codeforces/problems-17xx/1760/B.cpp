6654701d3c7654e3fb79e32ef400062350c3f526
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-11 11:06:31 +0100

    Solved Codeforces 1760B

---
+++ codeforces/problems-17xx/1760/B.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 835 (Div. 4)
+// B. Atilla's Favorite Problem
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        char max_character = *max_element(str.begin(), str.end());
+        cout << (max_character - 'a' + 1) << nl;
+    }
+    return 0;
+}
