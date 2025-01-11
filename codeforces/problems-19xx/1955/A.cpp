29029ad02efee551967666a6d318f70b92ec3f69
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-11 11:06:33 +0100

    Solved Codeforces 1955A

---
+++ codeforces/problems-19xx/1955/A.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 938 (Div. 3)
+// A. Yogurt Sale
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        int a;
+        cin >> a;
+        int b;
+        cin >> b;
+        cout << min(b * (n / 2) + a * (n % 2), a * n) << nl;
+    }
+    return 0;
+}
