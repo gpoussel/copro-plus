337047870f02df3f0c3d91649e3bc9d7fe3abd78
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-11 21:16:41 +0100

    Solved Codeforces 1348A

---
+++ codeforces/problems-13xx/1348/A.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 638 (Div. 2)
+// A. Phoenix and Balance
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        cout << (1 << (n / 2 + 1)) - 2 << nl;
+    }
+    return 0;
+}
