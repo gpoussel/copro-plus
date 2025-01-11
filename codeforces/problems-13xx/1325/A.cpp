67c7c4f54c57ad717edbc843f1411988da6c74b1
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-11 18:50:43 +0100

    Solved Codeforces 1325A

---
+++ codeforces/problems-13xx/1325/A.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 628 (Div. 2)
+// A. EhAb AnD gCd
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        long long x;
+        cin >> x;
+        cout << 1 << " " << x - 1 << nl;
+    }
+    return 0;
+}
