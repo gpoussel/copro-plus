16f8a388af03e3693c6d4e495abb90d2e0b36720
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-11 21:16:41 +0100

    Solved Codeforces 1337A

---
+++ codeforces/problems-13xx/1337/A.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 635 (Div. 2)
+// A. Ichihime and Triangle
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        long long a;
+        cin >> a;
+        long long b;
+        cin >> b;
+        long long c;
+        cin >> c;
+        long long d;
+        cin >> d;
+        cout << b << " " << c << " " << c << nl;
+    }
+    return 0;
+}
