4b9d42d3b07d3b3b1494269fc554159518b3c480
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-11 18:50:44 +0100

    Solved Codeforces 1454A

---
+++ codeforces/problems-14xx/1454/A.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 686 (Div. 3)
+// A. Special Permutation
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        for (int j = 2; j <= n; ++j)
+        {
+            cout << j << ' ';
+        }
+        cout << 1 << nl;
+    }
+    return 0;
+}
