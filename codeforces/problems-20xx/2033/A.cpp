567606978b8659590238cfb3b5895ba7306b8ace
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-12 10:38:41 +0100

    Solved Codeforces 2033A

---
+++ codeforces/problems-20xx/2033/A.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 981 (Div. 3)
+// A. Sakurako and Kosuke
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        if (n % 2 == 0)
+        {
+            cout << "Sakurako" << nl;
+        }
+        else
+        {
+            cout << "Kosuke" << nl;
+        }
+    }
+    return 0;
+}
