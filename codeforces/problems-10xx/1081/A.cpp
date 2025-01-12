50e0741e607986e89e1d6558fb4be2767ca8f47d
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-12 23:48:19 +0100

    Solved Codeforces 1081A

---
+++ codeforces/problems-10xx/1081/A.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Avito Cool Challenge 2018
+// A. Definite Game
+
+int main()
+{
+    long long number;
+    cin >> number;
+    cout << (number == 2 ? 2 : 1) << nl;
+    return 0;
+}
