e30014a46605134c8d5d7872334ee3bee932e5f0
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-12 16:05:33 +0100

    Solved Codeforces 1977A

---
+++ codeforces/problems-19xx/1977/A.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 948 (Div. 2)
+// A. Little Nikita
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        int m;
+        cin >> m;
+        if (m > n)
+        {
+            cout << "No" << nl;
+        }
+        else
+        {
+            cout << (n % 2 == m % 2 ? "Yes" : "No") << nl;
+        }
+    }
+    return 0;
+}
