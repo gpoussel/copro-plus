91043f99f0d6dec3b21154bf27860cf62e7bd0bc
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-12 10:38:40 +0100

    Solved Codeforces 1968A

---
+++ codeforces/problems-19xx/1968/A.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 943 (Div. 3)
+// A. Maximize?
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int x;
+        cin >> x;
+        cout << x - 1 << nl;
+    }
+    return 0;
+}
