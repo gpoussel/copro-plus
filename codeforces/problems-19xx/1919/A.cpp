e4d3d3dc27323dfa373e5c3ba9062631a38edda4
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-14 23:40:52 +0100

    Solved Codeforces 1919A

---
+++ codeforces/problems-19xx/1919/A.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Hello 2024
+// A. Wallet Exchange
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        long long a;
+        cin >> a;
+        long long b;
+        cin >> b;
+        if ((a + b) % 2 == 0)
+        {
+            cout << "Bob" << nl;
+        }
+        else
+        {
+            cout << "Alice" << nl;
+        }
+    }
+    return 0;
+}
