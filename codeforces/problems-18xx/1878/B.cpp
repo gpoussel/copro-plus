9aca7296963f832fe290857af319781e80c301f4
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-15 23:45:40 +0100

    Solved Codeforces 1878B

---
+++ codeforces/problems-18xx/1878/B.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 900 (Div. 3)
+// B. Aleksa and Stack
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        for (int j = 0; j < n; j++)
+        {
+            cout << (j > 0 ? " " : "") << 2 * j + 1;
+        }
+        cout << nl;
+    }
+    return 0;
+}
