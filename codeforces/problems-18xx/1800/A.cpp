da019c7ee51790f2256b3f1c75f93ce2dbcd37e4
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-15 23:45:39 +0100

    Solved Codeforces 1800A

---
+++ codeforces/problems-18xx/1800/A.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 855 (Div. 3)
+// A. Is It a Cat?
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        regex meow_regex("^m+e+o+w+$", regex_constants::icase);
+        cout << (regex_search(str, meow_regex) ? "YES" : "NO") << nl;
+    }
+    return 0;
+}
