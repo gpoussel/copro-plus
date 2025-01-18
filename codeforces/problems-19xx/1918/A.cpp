ada7f643fc757b2b738cfbe9d4448eb4ba10a85f
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-18 19:37:47 +0100

    Solved Codeforces 1918A

---
+++ codeforces/problems-19xx/1918/A.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 922 (Div. 2)
+// A. Brick Wall
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        int m;
+        cin >> m;
+        cout << n * (int)floor(m / 2.0) << nl;
+    }
+    return 0;
+}
