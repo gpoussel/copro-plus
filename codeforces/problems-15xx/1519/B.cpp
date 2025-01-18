d372da2a6667553be2284aec4884958657af1601
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-18 11:18:24 +0100

    Solved Codeforces 1519B

---
+++ codeforces/problems-15xx/1519/B.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Educational Codeforces Round 108 (Rated for Div. 2)
+// B. The Cake Is a Lie
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        int m;
+        cin >> m;
+        int k;
+        cin >> k;
+        cout << (n - 1 + (m - 1) * n == k ? "YES" : "NO") << nl;
+    }
+    return 0;
+}
