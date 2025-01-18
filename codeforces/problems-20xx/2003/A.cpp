5accd237e7f6c4014ac1b8e39e6a7500d76c4bcf
Guillaume Poussel <gpoussel@gmail.com>
2025-01-19 00:04:18 +0100

    Solved Codeforces 2003A

---
+++ codeforces/problems-20xx/2003/A.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 968 (Div. 2)
+// A. Turtle and Good Strings
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        cout << (str[0] != str[str.size() - 1] ? "YES" : "NO") << nl;
+    }
+    return 0;
+}
