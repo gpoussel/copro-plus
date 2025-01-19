655ee1dfe54315d80d0c0d4308dac002c8f5b25d
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-19 22:50:32 +0100

    Solved Codeforces 1896A

---
+++ codeforces/problems-18xx/1896/A.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - CodeTON Round 7 (Div. 1 + Div. 2, Rated, Prizes!)
+// A. Jagged Swaps
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        vector<int> arr(length);
+        for (int j = 0; j < length; j++)
+        {
+            cin >> arr[j];
+        }
+        cout << (arr[0] == 1 ? "YES" : "NO") << nl;
+    }
+    return 0;
+}
