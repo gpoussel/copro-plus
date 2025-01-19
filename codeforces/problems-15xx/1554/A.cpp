c9b036fe9718a0e0777334a8806bab8283a18d36
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-19 11:41:49 +0100

    Solved Codeforces 1554A

---
+++ codeforces/problems-15xx/1554/A.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 735 (Div. 2)
+// A. Cherry
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        vector<int> arr(length);
+        for (int j = 0; j < length; j++)
+        {
+            cin >> arr[j];
+        }
+        long long max_product = 0;
+        for (int j = 0; j < length - 1; ++j)
+        {
+            max_product = max(max_product, 1LL * arr[j] * arr[j + 1]);
+        }
+        cout << max_product << nl;
+    }
+    return 0;
+}
