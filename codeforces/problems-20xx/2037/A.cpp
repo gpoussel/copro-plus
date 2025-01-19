18e2d971c1947ce47765c8e90e6553ee2d48e7a8
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-19 13:58:53 +0100

    Solved Codeforces 2037A

---
+++ codeforces/problems-20xx/2037/A.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 988 (Div. 3)
+// A. Twice
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        map<int, int> frequency;
+        for (int j = 0; j < length; j++)
+        {
+            int number;
+            cin >> number;
+            frequency[number]++;
+        }
+        int result = 0;
+        for (auto &pair : frequency)
+        {
+            result += pair.second / 2;
+        }
+        cout << result << nl;
+    }
+    return 0;
+}
