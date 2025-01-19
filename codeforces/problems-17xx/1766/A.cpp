695f517b12b23dde2282ec6e9a17e9d0c6ae3f00
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-19 15:31:04 +0100

    Solved Codeforces 1766A

---
+++ codeforces/problems-17xx/1766/A.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Educational Codeforces Round 139 (Rated for Div. 2)
+// A. Extremely Round
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        int length = (int)log10(n) + 1;
+        int first_digit = n / (int)pow(10, length - 1);
+        cout << (length - 1) * 9 + first_digit << nl;
+    }
+    return 0;
+}
