8c6685b7bd57a60909766c050306fb4d1625fd58
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-19 22:50:29 +0100

    Solved Codeforces 1400A

---
+++ codeforces/problems-14xx/1400/A.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Educational Codeforces Round 94 (Rated for Div. 2)
+// A. String Similarity
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        for (int j = 0; j < n; ++j)
+        {
+            cout << str[2 * j];
+        }
+        cout << nl;
+    }
+    return 0;
+}
