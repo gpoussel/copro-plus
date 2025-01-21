9f1f256f1eb697ecae37e47efb4a14c6f1c863f4
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-21 21:35:19 +0100

    Solved Codeforces 1455A

---
+++ codeforces/problems-14xx/1455/A.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Educational Codeforces Round 99 (Rated for Div. 2)
+// A. Strange Functions
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        string str;
+        cin >> str;
+        cout << str.size() << nl;
+    }
+    return 0;
+}
