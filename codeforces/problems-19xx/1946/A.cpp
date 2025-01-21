80359ed7159b899bf162f55f95afe5ef8f932daa
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-21 21:35:21 +0100

    Solved Codeforces 1946A

---
+++ codeforces/problems-19xx/1946/A.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 936 (Div. 2)
+// A. Median of an Array
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        vector<long long> arr(length);
+        for (int j = 0; j < length; j++)
+        {
+            cin >> arr[j];
+        }
+        sort(arr.begin(), arr.end());
+        long long id = (long long)ceil((double)length / 2.0);
+        long long value = arr[id - 1];
+        int answer = 0;
+        while (id - 1 + answer < length && arr[id - 1 + answer] == value)
+        {
+            ++answer;
+        }
+        cout << answer << nl;
+    }
+    return 0;
+}
