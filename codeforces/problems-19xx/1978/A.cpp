a9029af7b1010fb00e8349c49aeef161efebb8d7
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-22 22:12:32 +0100

    Solved Codeforces 1978A

---
+++ codeforces/problems-19xx/1978/A.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 953 (Div. 2)
+// A. Alice and Books
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        vector<int> arr(length);
+        for (int j = 0; j < length; j++)
+        {
+            cin >> arr[j];
+        }
+        int max_without_last_element = *max_element(arr.begin(), arr.end() - 1);
+        int last_element = arr[length - 1];
+        cout << max_without_last_element + last_element << nl;
+    }
+    return 0;
+}
