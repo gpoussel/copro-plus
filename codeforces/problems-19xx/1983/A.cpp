e735b861ef4ca6ea7c8e1a0a446b68e1cc6ccb2c
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-22 22:12:32 +0100

    Solved Codeforces 1983A

---
+++ codeforces/problems-19xx/1983/A.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 956 (Div. 2) and ByteRace 2024
+// A. Array Divisibility
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; ++i)
+        {
+            cout << (i > 1 ? " " : "") << i;
+        }
+        cout << nl;
+    }
+    return 0;
+}
