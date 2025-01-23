4ad320e6c05d1d921d727c667f85e3044e79af4d
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-23 23:27:25 +0100

    Solved Codeforces 1987A

---
+++ codeforces/problems-19xx/1987/A.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - EPIC Institute of Technology Round Summer 2024 (Div. 1 + Div. 2)
+// A. Upload More RAM
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int n;
+        cin >> n;
+        int k;
+        cin >> k;
+        cout << n + (n - 1) * (k - 1) << nl;
+    }
+    return 0;
+}
