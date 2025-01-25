1a75699bb7a0bb900ac069f7c6b7e52281c9fdb2
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-25 19:39:22 +0100

    Solved Codeforces 1934A

---
+++ codeforces/problems-19xx/1934/A.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 931 (Div. 2)
+// A. Too Min Too Max
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        vector<int> arr(length);
+        for (int j = 0; j < length; j++)
+        {
+            cin >> arr[j];
+        }
+        sort(arr.begin(), arr.end());
+        cout << 2 * (arr[length - 1] - arr[0]) + 2 * (arr[length - 2] - arr[1]) << nl;
+    }
+    return 0;
+}
