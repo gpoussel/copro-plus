8e25c075bce59e34dcc52922eb014ef7d6c69273
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-26 22:50:04 +0100

    Solved Codeforces 2062A

---
+++ codeforces/problems-20xx/2062/A.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Ethflow Round 1 (Codeforces Round 1001, Div. 1 + Div. 2)
+// A. String
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        string str;
+        cin >> str;
+        cout << count(str.begin(), str.end(), '1') << nl;
+    }
+    return 0;
+}
