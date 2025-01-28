6d292b4567d0fb916c58faf1eb9b5f0e7dfcc432
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-28 21:06:10 +0100

    Solved Codeforces 1719A

---
+++ codeforces/problems-17xx/1719/A.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 814 (Div. 2)
+// A. Chip Game
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        long long n;
+        cin >> n;
+        long long m;
+        cin >> m;
+        cout << ((n % 2 == m % 2) ? "Tonya" : "Burenka") << nl;
+    }
+    return 0;
+}
