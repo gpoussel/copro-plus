6f36ca5f171c88e29b81e69893741e26fad8f12f
Guillaume POUSSEL <gpoussel@gmail.com>
2025-01-28 21:06:11 +0100

    Solved Codeforces 2003B

---
+++ codeforces/problems-20xx/2003/B.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define nl '\n'
+#define deb(x) cerr << #x " = " << x << nl
+#define EPSILON 1e-9
+
+// Codeforces - Codeforces Round 968 (Div. 2)
+// B. Turtle and Piggy Are Playing a Game 2
+
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        int length;
+        cin >> length;
+        vector<int> arr(length);
+        for (int j = 0; j < length; j++)
+        {
+            cin >> arr[j];
+        }
+        sort(arr.begin(), arr.end());
+        cout << arr[length / 2] << nl;
+    }
+    return 0;
+}
